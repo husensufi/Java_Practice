66484ca1f3e9e844b65242efb1e3148fe2d61ae5
Husen Sufi <mahammadshafeermangalore@gmail.com>
2025-10-02 01:02:14 +0530

    SumEvenOrOdd Program

--- Day01/SumEvenOrOdd.java
+++ Day01/SumEvenOrOdd.java
@@ -1,2 +1,27 @@
 //WAP TO CHECK WHETHER SUM OF NUMBER BETWEEN IN RANGE IS EVEN OR ODD
 
+import java.util.Scanner;
+
+public class SumEvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number to be calculated as a Sum...");
+        int total = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= total; i++) {
+            System.out.println("Enter a " + i + "th Value");
+            int num = scanner.nextInt();
+
+            sum = sum + num;
+
+        }
+        if (sum % 2 == 0) {
+            System.out.println(sum + " The Sum of Number is Even...");
+        }
+        else {
+        System.out.println(sum + " The Sum of Number is Odd...");
+       }
+    }
+}
